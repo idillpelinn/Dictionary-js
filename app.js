8b3106f11b58fecb8696cdcf11cf6827d6442fff
Pelin İdil Guzel <idilgzl75@gmail.com>
2023-10-16 13:51:49 +0300

    change api

--- app.js
+++ app.js
@@ -5,13 +5,37 @@ const result = document.querySelector(".result");
 const sound = document.getElementById("sound");
 const search_btn = document.getElementById("search");
 
-search_btn.addEventListener("click", ()=>{
 
-    let input_word = document.getElementById("input-word").value;
-    fetch(`${url}${input_word}`)
-    .then((response)=> response.json)
-    .then((data) =>console.log(data))
-
-
-})
-
+search_btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("input-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+                sound.setAttribute("src", `${data[0].phonetics[1].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+        
+});
+function playSound() {
+    sound.play();
+}
